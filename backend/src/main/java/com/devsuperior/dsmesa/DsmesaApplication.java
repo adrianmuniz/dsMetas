a6302adfb2dc73aea6830bfd1ea076ad0adf1531
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-07-13 22:40:38 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmesa/DsmesaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmesaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmesa/DsmesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
